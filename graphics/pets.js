5557a34307b3c63c561ced14fbf565c3bc70c920
Timothy Yang <timothyoung97@gmail.com>
2022-01-08 02:00:10 +0800

    Update pets.js

--- graphics/pets.js
+++ graphics/pets.js
@@ -44,6 +44,13 @@ function drawBat(colour) {
 
     c.fillRect(300, 330, 10, 10);
     c.fillRect(350, 330, 10, 10);
+
+    c.fillStyle = 'white';
+    c.fillRect(320, 380, 10, 10);
+    c.fillRect(330, 380, 10, 10);
+
+    c.fillRect(310, 360, 10, 10);
+    c.fillRect(340, 360, 10, 10);
 }
 
 function drawDog(colour) {
@@ -85,6 +92,10 @@ function drawDog(colour) {
     c.fillRect(300, 340, 10, 10);
     c.fillRect(320, 340, 10, 10);
     c.fillRect(340, 340, 10, 10);
+
+    c.fillStyle = 'white';
+    c.fillRect(310, 360, 10, 10);
+    c.fillRect(330, 360, 10, 10);
 }
 
 function drawYat(colour) {
@@ -126,6 +137,14 @@ function drawYat(colour) {
 
     c.fillRect(310, 340, 10, 10);
     c.fillRect(340, 340, 10, 10);
+
+    c.fillStyle = 'white';
+    c.fillRect(320, 380, 10, 10);
+    c.fillRect(330, 380, 10, 10);
+
+    c.fillRect(310, 360, 10, 10);
+    c.fillRect(340, 360, 10, 10);
+
 }
 
 function drawDat(colour) {
@@ -171,6 +190,14 @@ function drawDat(colour) {
 
     c.fillRect(320, 330, 10, 10);
     c.fillRect(320, 320, 10, 10);
+
+    c.fillStyle = 'white';
+    c.fillRect(310, 380, 10, 10);
+    c.fillRect(320, 380, 10, 10);
+    c.fillRect(330, 380, 10, 10);
+
+    c.fillRect(310, 360, 10, 10);
+    c.fillRect(330, 360, 10, 10);
 }
 
 function drawAat(colour) {
@@ -211,9 +238,82 @@ function drawAat(colour) {
 
     c.fillRect(300, 330, 10, 10);
     c.fillRect(350, 330, 10, 10);
+
+    c.fillStyle = 'white';
+    c.fillRect(310, 360, 10, 10);
+    c.fillRect(340, 360, 10, 10);
 }
 
 function drawPat(colour) {
+    c.fillStyle = 'white';
+    c.fillRect(290, 390, 10, 10);
+    c.fillRect(300, 390, 10, 10);
+    c.fillRect(310, 390, 10, 10);
+    c.fillRect(350, 390, 10, 10);
+    c.fillRect(360, 390, 10, 10);
+
+    c.fillRect(290, 380, 10, 10);
+    c.fillRect(300, 380, 10, 10);
+    c.fillRect(310, 380, 10, 10);
+    c.fillRect(350, 380, 10, 10);
+    c.fillRect(360, 380, 10, 10);
+
+    c.fillRect(290, 370, 10, 10);
+    c.fillRect(300, 370, 10, 10);
+    c.fillRect(310, 370, 10, 10);
+    c.fillRect(320, 370, 10, 10);
+    c.fillRect(330, 370, 10, 10);
+    c.fillRect(340, 370, 10, 10);
+    c.fillRect(350, 370, 10, 10);
+    c.fillRect(360, 370, 10, 10);
+
+    c.fillRect(290, 360, 10, 10);
+    c.fillRect(300, 360, 10, 10);
+    c.fillRect(310, 360, 10, 10);
+    c.fillRect(320, 360, 10, 10);
+    c.fillRect(330, 360, 10, 10);
+    c.fillRect(340, 360, 10, 10);
+    c.fillRect(350, 360, 10, 10);
+    c.fillRect(360, 360, 10, 10);
+
+    c.fillRect(290, 350, 10, 10);
+    c.fillRect(300, 350, 10, 10);
+    c.fillRect(310, 350, 10, 10);
+    c.fillRect(320, 350, 10, 10);
+    c.fillRect(330, 350, 10, 10);
+    c.fillRect(340, 350, 10, 10);
+    c.fillRect(350, 350, 10, 10);
+    c.fillRect(360, 350, 10, 10);
+
+    c.fillRect(280, 340, 10, 10);
+    c.fillRect(290, 340, 10, 10);
+    c.fillRect(300, 340, 10, 10);
+    c.fillRect(310, 340, 10, 10);
+    c.fillRect(320, 340, 10, 10);
+    c.fillRect(330, 340, 10, 10);
+    c.fillRect(340, 340, 10, 10);
+    c.fillRect(350, 340, 10, 10);
+    c.fillRect(360, 340, 10, 10);
+    c.fillRect(370, 340, 10, 10);
+
+    c.fillRect(290, 330, 10, 10);
+    c.fillRect(300, 330, 10, 10);
+    c.fillRect(310, 330, 10, 10);
+    c.fillRect(320, 330, 10, 10);
+    c.fillRect(330, 330, 10, 10);
+    c.fillRect(340, 330, 10, 10);
+    c.fillRect(350, 330, 10, 10);
+    c.fillRect(360, 330, 10, 10);
+    
+    c.fillRect(300, 320, 10, 10);
+    c.fillRect(310, 320, 10, 10);
+    c.fillRect(320, 320, 10, 10);
+    c.fillRect(330, 320, 10, 10);
+    c.fillRect(340, 320, 10, 10);
+    c.fillRect(350, 320, 10, 10);
+    c.fillRect(360, 320, 10, 10);
+    c.fillRect(370, 320, 10, 10);
+
     c.fillStyle = colour;
     c.fillRect(290, 390, 10, 10);
     c.fillRect(300, 390, 10, 10);
@@ -271,6 +371,7 @@ function drawPat(colour) {
     c.fillRect(350, 300, 10, 10);
     c.fillRect(360, 300, 10, 10);
     c.fillRect(370, 300, 10, 10);
+
 }
 
 var pets = {
